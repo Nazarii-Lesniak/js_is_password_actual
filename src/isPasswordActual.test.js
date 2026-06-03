593343ee7b11561700f967f8557897b6c0f52edf
Nazarii-Lesniak <naziksay@gmail.com>
2026-06-03 21:00:48 +0300

    test: add unit tests for isPasswordActual

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -13,13 +13,27 @@ describe(`Function 'isPasswordActual':`, () => {
     expect(isPasswordActual).toBeInstanceOf(Function);
   });
 
-  it(`should return a string`, () => {});
+  it(`should return a string`, () => {
+    expect(typeof isPasswordActual(today.year, today.month, today.date)).toBe(
+      'string',
+    );
+  });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
+  it(`should return immediate change status for password changed a year ago`, () => {
     const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
 
     expect(lastYear).toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should return warning status for password changed a month ago`, () => {
+    const lastMonth = isPasswordActual(today.year, today.month - 1, today.date);
+
+    expect(lastMonth).toBe('You should change your password.');
+  });
+
+  it(`should return actual status for password changed 30 days ago`, () => {
+    const lastDay = isPasswordActual(today.year, today.month, today.date - 30);
+
+    expect(lastDay).toBe('Password is actual.');
+  });
 });
