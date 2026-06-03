d684bb168686bdfa8696c5d7379da0ac36dd2317
Nazarii-Lesniak <naziksay@gmail.com>
2026-06-03 21:14:28 +0300

    test: fix January boundary bug and update test names

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -19,19 +19,29 @@ describe(`Function 'isPasswordActual':`, () => {
     );
   });
 
-  it(`should return immediate change status for password changed a year ago`, () => {
-    const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
+  it(`should return immediate change status if changed more than 60 days ago`, () => {
+    const lastYear = isPasswordActual(today.year, today.month, today.date - 61);
 
     expect(lastYear).toBe('Immediately change the password!');
   });
 
-  it(`should return warning status for password changed a month ago`, () => {
-    const lastMonth = isPasswordActual(today.year, today.month - 1, today.date);
+  it(`should return warning status if changed between 31 and 60 days ago`, () => {
+    const lowerBoundaryCase = isPasswordActual(
+      today.year,
+      today.month,
+      today.date - 60,
+    );
+    const upperBoundaryCase = isPasswordActual(
+      today.year,
+      today.month,
+      today.date - 31,
+    );
 
-    expect(lastMonth).toBe('You should change your password.');
+    expect(lowerBoundaryCase).toBe('You should change your password.');
+    expect(upperBoundaryCase).toBe('You should change your password.');
   });
 
-  it(`should return actual status for password changed 30 days ago`, () => {
+  it(`should return actual status if changed 30 days ago or less`, () => {
     const lastDay = isPasswordActual(today.year, today.month, today.date - 30);
 
     expect(lastDay).toBe('Password is actual.');
